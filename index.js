d9c3acd178971ae31fcf3dc3a15dc47aebe44415
Susan <suzutoyuki@gmail.com>
2018-02-17 09:48:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ function createRock(x) {
    * Now that we have a rock, we'll need to append
    * it to GAME and move it downwards.
    */
-GAME.appendchild(rock)
+GAME.appendChild(rock)
 
   /**
    * This function moves the rock. (2 pixels at a time
